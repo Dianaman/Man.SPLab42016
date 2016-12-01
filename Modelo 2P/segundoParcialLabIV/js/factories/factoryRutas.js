c87ad49a9a514962fe5bbf6bebcce640585933ca
dianaman <diana.man.91@gmail.com>
2016-12-01 18:45:51 -0300

    base 2p

---
+++ Modelo 2P/segundoParcialLabIV/js/factories/factoryRutas.js
@@ -0,0 +1,16 @@
+angular
+  .module('app')
+  .factory('factoryRutas', function (){
+  	var objeto = {};
+
+    objeto.nombre = "Factory de rutas";
+    objeto.RutaProductos = 'http://localhost/2ParcialLab/webservice1/productos/';
+    //objeto.RutaProductos = 'http://localhost/webservice1/productos/';
+
+    objeto.RutaUsuarios = 'http://localhost/2ParcialLab/webservice1/usuarios/';
+
+    return objeto;
+
+
+    
+  })
---
+++ Modelo 2P/segundoParcialLabIV/js/factories/factoryUsuario.js
@@ -0,0 +1,40 @@
+angular
+	.module('app')
+	.factory('UsuarioActual', [function(){
+
+		var nombre = "";
+		var email = "";
+		var tipo = "";
+
+		return {
+
+			login:function(name,mail,type){
+
+				nombre = name;
+				email = mail;
+				tipo = type;
+
+			},
+			logout:function(){
+				nombre = "";
+				email = "";
+				tipo = "";
+			},
+			getName:function(){
+				return nombre;
+			},getEmail:function(){
+				return email;
+			},getTipo:function(){
+				return tipo;
+			},getFullData:function(){
+				var jsonUsuario = {};
+				jsonUsuario.nombre = nombre;
+				jsonUsuario.email = email;
+				jsonUsuario.tipo = tipo;
+
+				return JSON.stringify(jsonUsuario);
+			}
+
+		};
+
+	}])
